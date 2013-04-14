7429027b621a6dead3d2cb30c0263aa00ed704fe
ljw1811 <ljw1811@126.com>
2013-04-14 21:49:41 +0800

    test

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	/**   
+	 * main(这里用一句话描述这个方法的作用)  
+	 * (这里描述这个方法适用条件 – 可选)  
+	 * @param args void
+	 * @auther Jianpin.Li  
+	 * @exception    
+	 * @since  1.0.0   
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
